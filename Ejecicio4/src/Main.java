3a4f1757503546d69783caa49a9085fd903674de
JeanPierreCrespin <crespinpierre98@gmail.com>
2022-07-30 11:27:19 -0300

    ejercicio4.2

--- Ejecicio4/src/Main.java
+++ Ejecicio4/src/Main.java
@@ -1,2 +1,47 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        int numeroIf = 20;
+        int numeroWhile = 10;
+        int numeroFor = 0;
+        String estacion = "verano";
+
+        if(numeroIf > 0){
+            System.out.println("El número es positivo");
+        }else{
+            System.out.println("El número es negativo");
+        }
+
+        while (numeroWhile<3) {
+            numeroWhile ++;
+            System.out.println(numeroWhile);
+        }
+
+        do{
+            numeroWhile++;
+            System.out.println(numeroWhile);
+        }while(numeroWhile <3);
+
+        for (int i = 0; i<3; i ++){
+            numeroFor++;
+            System.out.println(numeroFor);
+        }
+
+        switch (estacion){
+            case "verano":
+                System.out.println("Es Verano");
+                break;
+            case "otoño":
+                System.out.println("Es otoño");
+                break;
+            case "invierno":
+                System.out.println("Es Invierno");
+                break;
+            case "primavera":
+                System.out.println("Es Privera");
+                break;
+
+            default:
+                System.out.println("Opción incorrecta");
+        }
+    }
 }
